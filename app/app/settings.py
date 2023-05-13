8f3945fd5992c58ec9a8e86af3786154864bdebe
BhavyaSanamgi <bhavya.sanamgi@gmail.com>
2023-05-13 15:36:07 +0530

    added wait for db

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -77,10 +78,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'HOST': os.environ.get('DB_HOST'),
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD':  os.environ.get('DB_PASS')
+        'HOST': 'localhost',
+        'NAME': 'recipe-app-api',
+        'USER': 'postgres',
+        'PASSWORD':  'Fullstack@2023',
+        'PORT': '5433'
     }
 }
 
